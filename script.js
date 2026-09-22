63608b5c054f263a3ffb0b052cecdb043264e4df
bppnsarawak <bppnsarawak@gmail.com>
2026-09-22 23:08:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* ================================
    CONFIG
 ================================ */
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxR3_FacfMCznalRpHdCrRn1ypsEI90T2xNH-Ll0DXp28TM9pn34XYCp897ibZYjbDA/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyp-xJ5KzR-PLgHdJszTqDPQ4OHq3Hw_LtAiHS3g4Ty-i7Iowly45IN3vmh0YNZ0A/exec";
 
 const TABLE_PRICE = 2000;
 const CHAIR_PRICE = 200;
